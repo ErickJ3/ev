c3e3e69fcba845ca3e501e30295a659f65f5e432
Erick Jesus <erick.jesus2060@gmail.com>
2026-02-22 01:20:22 -0300

    ci

--- build.zig
+++ build.zig
@@ -10,36 +10,76 @@ pub fn build(b: *std.Build) void {
         .target = target,
     });
 
-    const clap_dep = b.dependency("clap", .{});
-    const clap_mod = clap_dep.module("clap");
-
     const version_options = b.addOptions();
     version_options.addOption([]const u8, "version", zon.version);
 
-    const exe = b.addExecutable(.{
-        .name = "ev",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-            .imports = &.{
-                .{ .name = "evi", .module = mod },
-                .{ .name = "clap", .module = clap_mod },
-                .{ .name = "build_options", .module = version_options.createModule() },
-            },
-        }),
-    });
+    const clap_mod = if (b.lazyDependency("clap", .{})) |dep| dep.module("clap") else null;
 
-    b.installArtifact(exe);
+    if (clap_mod) |cm| {
+        const exe = b.addExecutable(.{
+            .name = "ev",
+            .root_module = b.createModule(.{
+                .root_source_file = b.path("src/main.zig"),
+                .target = target,
+                .optimize = optimize,
+                .imports = &.{
+                    .{ .name = "evi", .module = mod },
+                    .{ .name = "clap", .module = cm },
+                    .{ .name = "build_options", .module = version_options.createModule() },
+                },
+            }),
+        });
 
-    const run_step = b.step("run", "Run the app");
-    const run_cmd = b.addRunArtifact(exe);
+        b.installArtifact(exe);
 
-    run_step.dependOn(&run_cmd.step);
-    run_cmd.step.dependOn(b.getInstallStep());
+        const run_step = b.step("run", "Run the app");
+        const run_cmd = b.addRunArtifact(exe);
 
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
+        run_step.dependOn(&run_cmd.step);
+        run_cmd.step.dependOn(b.getInstallStep());
+
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+
+        const release_step = b.step("release", "Build release binaries for all targets");
+
+        const release_targets = [_]struct {
+            query: std.Target.Query,
+            name: []const u8,
+        }{
+            .{ .query = .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu }, .name = "x86_64-linux" },
+            .{ .query = .{ .cpu_arch = .aarch64, .os_tag = .linux, .abi = .gnu }, .name = "aarch64-linux" },
+            .{ .query = .{ .cpu_arch = .x86_64, .os_tag = .freebsd }, .name = "x86_64-freebsd" },
+        };
+
+        for (release_targets) |rt| {
+            const resolved = b.resolveTargetQuery(rt.query);
+
+            const release_evi_mod = b.createModule(.{
+                .root_source_file = b.path("src/root.zig"),
+                .target = resolved,
+            });
+
+            const release_exe = b.addExecutable(.{
+                .name = "ev",
+                .root_module = b.createModule(.{
+                    .root_source_file = b.path("src/main.zig"),
+                    .target = resolved,
+                    .optimize = .ReleaseSafe,
+                    .imports = &.{
+                        .{ .name = "evi", .module = release_evi_mod },
+                        .{ .name = "clap", .module = cm },
+                        .{ .name = "build_options", .module = version_options.createModule() },
+                    },
+                }),
+            });
+
+            const install = b.addInstallArtifact(release_exe, .{
+                .dest_dir = .{ .override = .{ .custom = b.fmt("release/{s}", .{rt.name}) } },
+            });
+            release_step.dependOn(&install.step);
+        }
     }
 
     const mod_tests = b.addTest(.{
@@ -49,43 +89,4 @@ pub fn build(b: *std.Build) void {
     const run_mod_tests = b.addRunArtifact(mod_tests);
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&run_mod_tests.step);
-
-    const release_step = b.step("release", "Build release binaries for all targets");
-
-    const release_targets = [_]struct {
-        query: std.Target.Query,
-        name: []const u8,
-    }{
-        .{ .query = .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu }, .name = "x86_64-linux" },
-        .{ .query = .{ .cpu_arch = .aarch64, .os_tag = .linux, .abi = .gnu }, .name = "aarch64-linux" },
-        .{ .query = .{ .cpu_arch = .x86_64, .os_tag = .freebsd }, .name = "x86_64-freebsd" },
-    };
-
-    for (release_targets) |rt| {
-        const resolved = b.resolveTargetQuery(rt.query);
-
-        const release_evi_mod = b.createModule(.{
-            .root_source_file = b.path("src/root.zig"),
-            .target = resolved,
-        });
-
-        const release_exe = b.addExecutable(.{
-            .name = "ev",
-            .root_module = b.createModule(.{
-                .root_source_file = b.path("src/main.zig"),
-                .target = resolved,
-                .optimize = .ReleaseSafe,
-                .imports = &.{
-                    .{ .name = "evi", .module = release_evi_mod },
-                    .{ .name = "clap", .module = clap_mod },
-                    .{ .name = "build_options", .module = version_options.createModule() },
-                },
-            }),
-        });
-
-        const install = b.addInstallArtifact(release_exe, .{
-            .dest_dir = .{ .override = .{ .custom = b.fmt("release/{s}", .{rt.name}) } },
-        });
-        release_step.dependOn(&install.step);
-    }
 }
